895546b1d05cf89433d674d6dac9900f3f881da6
shnhrrsn <shnhrrsn@gmail.com>
2009-10-19 22:58:25 -0400

    Added license

--- EGOCache.h
+++ EGOCache.h
@@ -5,6 +5,10 @@
 //  Created by Shaun Harrison on 7/4/09.
 //  Copyright 2009 enormego. All rights reserved.
 //
+//  This work is licensed under the Creative Commons GNU General Public License License.
+//  To view a copy of this license, visit http://creativecommons.org/licenses/GPL/2.0/
+//  or send a letter to Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
+//
 
 #import <Foundation/Foundation.h>
 
@@ -15,6 +19,7 @@
 }
 
 + (EGOCache*)currentCache;
+- (void)clearCache; // This is blocking, and may take a while.
 
 - (BOOL)hasCacheForKey:(NSString*)key;
 
--- EGOCache.m
+++ EGOCache.m
@@ -5,6 +5,10 @@
 //  Created by Shaun Harrison on 7/4/09.
 //  Copyright 2009 enormego. All rights reserved.
 //
+//  This work is licensed under the Creative Commons GNU General Public License License.
+//  To view a copy of this license, visit http://creativecommons.org/licenses/GPL/2.0/
+//  or send a letter to Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
+//
 
 #import "EGOCache.h"
 
@@ -65,6 +69,16 @@ static id __instance;
 	}
 	
 	return self;
+}
+
+- (void)clearCache {
+	for(NSString* key in cacheDictionary) {
+		[[NSFileManager defaultManager] removeItemAtPath:cachePathForKey(key) error:NULL];
+	}
+
+	[cacheDictionary removeAllObjects];
+	[[NSUserDefaults standardUserDefaults] setObject:cacheDictionary forKey:@"EGOCache"];
+	[[NSUserDefaults standardUserDefaults] synchronize];
 }
 
 - (BOOL)hasCacheForKey:(NSString*)key {
