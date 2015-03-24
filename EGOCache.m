f4a3e855d94560825dfb3c1285621c5e0ee565cc
Shaun Harrison <shnhrrsn@gmail.com>
2015-03-24 13:30:07 -0400

    A little tidying up

--- EGOCache.m
+++ EGOCache.m
@@ -2,8 +2,8 @@
 //  EGOCache.m
 //  enormego
 //
-//  Created by Shaun Harrison on 7/4/09.
-//  Copyright (c) 2009-2012 enormego
+//  Created by Shaun Harrison.
+//  Copyright (c) 2009-2015 enormego.
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
@@ -27,11 +27,11 @@
 #import "EGOCache.h"
 
 #if DEBUG
-#define CHECK_FOR_EGOCACHE_PLIST() if([key isEqualToString:@"EGOCache.plist"]) { \
-NSLog(@"EGOCache.plist is a reserved key and can not be modified."); \
-return; }
+#	define CHECK_FOR_EGOCACHE_PLIST() if([key isEqualToString:@"EGOCache.plist"]) { \
+		NSLog(@"EGOCache.plist is a reserved key and can not be modified."); \
+		return; }
 #else
-#define CHECK_FOR_EGOCACHE_PLIST() if([key isEqualToString:@"EGOCache.plist"]) return;
+#	define CHECK_FOR_EGOCACHE_PLIST() if([key isEqualToString:@"EGOCache.plist"]) return;
 #endif
 
 static inline NSString* cachePathForKey(NSString* directory, NSString* key) {
@@ -70,7 +70,7 @@ static inline NSString* cachePathForKey(NSString* directory, NSString* key) {
 	return instance;
 }
 
-- (id)init {
+- (instancetype)init {
 	NSString* cachesDirectory = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES)[0];
 	NSString* oldCachesDirectory = [[[cachesDirectory stringByAppendingPathComponent:[[NSProcessInfo processInfo] processName]] stringByAppendingPathComponent:@"EGOCache"] copy];
 
@@ -82,9 +82,8 @@ static inline NSString* cachePathForKey(NSString* directory, NSString* key) {
 	return [self initWithCacheDirectory:cachesDirectory];
 }
 
-- (id)initWithCacheDirectory:(NSString*)cacheDirectory {
+- (instancetype)initWithCacheDirectory:(NSString*)cacheDirectory {
 	if((self = [super init])) {
-
 		_cacheInfoQueue = dispatch_queue_create("com.enormego.egocache.info", DISPATCH_QUEUE_SERIAL);
 		dispatch_queue_t priority = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0);
 		dispatch_set_target_queue(priority, _cacheInfoQueue);
@@ -153,9 +152,9 @@ static inline NSString* cachePathForKey(NSString* directory, NSString* key) {
 }
 
 - (BOOL)hasCacheForKey:(NSString*)key {
-    NSDate* date = [self dateForKey:key];
-	if(!date) return NO;
-	if([date compare:[NSDate date]] != NSOrderedDescending) return NO;
+	NSDate* date = [self dateForKey:key];
+	if(date == nil) return NO;
+	if([date timeIntervalSinceReferenceDate] < CFAbsoluteTimeGetCurrent()) return NO;
 	
 	return [[NSFileManager defaultManager] fileExistsAtPath:cachePathForKey(_directory, key)];
 }
@@ -167,17 +166,17 @@ static inline NSString* cachePathForKey(NSString* directory, NSString* key) {
 		date = (self.frozenCacheInfo)[key];
 	});
 
-    return date;
+	return date;
 }
 
 - (NSArray*)allKeys {
-    __block NSArray* keys = nil;
+	__block NSArray* keys = nil;
 
-    dispatch_sync(_frozenCacheInfoQueue, ^{
-        keys = [self.frozenCacheInfo allKeys];
-    });
+	dispatch_sync(_frozenCacheInfoQueue, ^{
+		keys = [self.frozenCacheInfo allKeys];
+	});
 
-    return keys;
+	return keys;
 }
 
 - (void)setCacheTimeoutInterval:(NSTimeInterval)timeoutInterval forKey:(NSString*)key {
@@ -195,7 +194,6 @@ static inline NSString* cachePathForKey(NSString* directory, NSString* key) {
 		
 		self.frozenCacheInfo = info;
 	});
-	
 	
 	// Save the final copy (this may be blocked by other operations)
 	dispatch_async(_cacheInfoQueue, ^{
@@ -327,8 +325,7 @@ static inline NSString* cachePathForKey(NSString* directory, NSString* key) {
 }
 
 - (void)setImage:(NSImage*)anImage forKey:(NSString*)key withTimeoutInterval:(NSTimeInterval)timeoutInterval {
-	[self setData:[[[anImage representations] objectAtIndex:0] representationUsingType:NSPNGFileType properties:nil]
-		   forKey:key withTimeoutInterval:timeoutInterval];
+	[self setData:[[[anImage representations] objectAtIndex:0] representationUsingType:NSPNGFileType properties:nil] forKey:key withTimeoutInterval:timeoutInterval];
 }
 
 #endif
@@ -338,10 +335,7 @@ static inline NSString* cachePathForKey(NSString* directory, NSString* key) {
 
 - (NSData*)plistForKey:(NSString*)key; {  
 	NSData* plistData = [self dataForKey:key];
-    return [NSPropertyListSerialization propertyListWithData:plistData
-                                                     options:NSPropertyListImmutable
-                                                      format:nil
-                                                       error:nil];
+	return [NSPropertyListSerialization propertyListWithData:plistData options:NSPropertyListImmutable format:nil error:nil];
 }
 
 - (void)setPlist:(id)plistObject forKey:(NSString*)key; {
@@ -350,12 +344,11 @@ static inline NSString* cachePathForKey(NSString* directory, NSString* key) {
 
 - (void)setPlist:(id)plistObject forKey:(NSString*)key withTimeoutInterval:(NSTimeInterval)timeoutInterval; {
 	// Binary plists are used over XML for better performance
-    NSData* plistData = [NSPropertyListSerialization dataWithPropertyList:plistObject
-                                                                   format:NSPropertyListBinaryFormat_v1_0
-                                                                  options:0
-                                                                    error:nil];
-    
-	[self setData:plistData forKey:key withTimeoutInterval:timeoutInterval];
+	NSData* plistData = [NSPropertyListSerialization dataWithPropertyList:plistObject format:NSPropertyListBinaryFormat_v1_0 options:0 error:nil];
+
+	if(plistData != nil) {
+		[self setData:plistData forKey:key withTimeoutInterval:timeoutInterval];
+	}
 }
 
 #pragma mark -
@@ -375,12 +368,6 @@ static inline NSString* cachePathForKey(NSString* directory, NSString* key) {
 
 - (void)setObject:(id<NSCoding>)anObject forKey:(NSString*)key withTimeoutInterval:(NSTimeInterval)timeoutInterval {
 	[self setData:[NSKeyedArchiver archivedDataWithRootObject:anObject] forKey:key withTimeoutInterval:timeoutInterval];
-}
-
-#pragma mark -
-
-- (void)dealloc {
-
 }
 
 @end
